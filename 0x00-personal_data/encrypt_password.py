e79364a487a076f4351fb2d77cd04411c0590963
youssefelghamour <youssefelghamour@gmail.com>
2024-07-04 22:00:19 +0000

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,4 +5,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if the provided password matches the hashed password """
+    return bcrypt.checkpw(password.encode(), hashed_password)

a504c684576abd72cbb47fa5cfcea9a1b24839a0
youssefelghamour <youssefelghamour@gmail.com>
2024-07-04 16:17:53 +0000

    correct filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ module for obfuscating sensitive data in log messages """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ function filter sensitive fields in a log message """
     return re.sub(r'({})=([^{}]+)'.format('|'.join(fields), separator),
                   r'\1={}'.format(redaction), message)

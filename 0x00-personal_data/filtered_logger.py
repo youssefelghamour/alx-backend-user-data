e1a848d91e08da20b58ad6f183d40fd2cd40d2f0
youssefelghamour <youssefelghamour@gmail.com>
2024-07-04 16:12:00 +0000

    correct filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,16 +3,8 @@
 import re
 
 
-# () capture group that captures a specific pattern
-# [] matches any one of the characters inside the brackets
-# in this case, ^; translate literally to anything except semicolon
-# (since ^ matches any character not listed in the brackets)
-# + to capture more than one character (all chracters until we reach ;)
-
-
 def filter_datum(fields, redaction, message, separator):
     """ function filter sensitive fields in a log message """
-    for field in fields:
-        pattern = r'{}=([^{}{}]+)'.format(field, separator, separator)
-        message = re.sub(pattern, '{}={}'.format(field, redaction), message)
-    return message
+    return re.sub(r'({})=([^{}]+)'.format('|'.join(fields), separator),
+                  r'\1={}'.format(redaction),
+                  message)

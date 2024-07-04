c7b167156ba334f8439eeb3d07c9378580a6ce4e
youssefelghamour <youssefelghamour@gmail.com>
2024-07-04 16:00:44 +0000

    correct filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,22 +11,9 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ function filter sensitive fields in a log message
-        and obfuscate their values
-
-        Arguments:
-        - fields: a list of strings representing all fields to obfuscate
-        - redaction: a string representing by what the field will be obfuscated
-        - message: a string representing the log line
-        - separator: a string representing by which character is separating all
-                    fields in the log line (message)
-
-        Returns:
-        - modified_message: the log message with sensitive fields obfuscated
-    """
-    modified_message = message
+    """ function filter sensitive fields in a log message """
     for field in fields:
         pattern = r'{}=([^{}]+)'.format(field, separator)
         modified_message = re.sub(pattern, '{}={}'.format(field, redaction),
-                                  modified_message)
-    return modified_message
+                                  message)
+    return message

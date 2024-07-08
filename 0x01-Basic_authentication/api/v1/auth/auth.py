4f13470022686553393e040e1a74c193ba86f8bc
youssefelghamour <youssefelghamour@gmail.com>
2024-07-08 14:53:58 +0000

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,12 @@ class Auth:
     """ Authentification class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if authentication is required for the given path """
+        """ Checks if authentication is required for the given path
+
+            - excluded_paths: Paths that do not require authentication
+
+            Returns: True if the path requires authentification, False otherwise
+        """
         if not path or not excluded_paths:
             return True
 
@@ -25,8 +30,15 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Placeholder method for retrieving authorization header """
-        return None
+        """ Retrieves the authorization header """
+        if not request:
+            return None
+
+        headers = request.headers
+        if 'Authorization' not in headers.keys():
+            return None
+
+        return headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Placeholder method for retrieving current user """

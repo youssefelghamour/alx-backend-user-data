de9007bb9e371a009b8515a4527d6460b5a32c93
youssefelghamour <youssefelghamour@gmail.com>
2024-07-08 17:39:26 +0000

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,3 +17,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64 string """
+        if (not base64_authorization_header
+                or not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+        except base64.binascii.Error as e:
+            return None
+
+        decoded_string = decoded_bytes.decode('utf-8')
+        return decoded_string

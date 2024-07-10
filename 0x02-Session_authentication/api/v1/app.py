37b3333202fc9385b48e469f8170d7933c6da852
youssefelghamour <youssefelghamour@gmail.com>
2024-07-10 17:58:47 +0000

    1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -22,6 +23,8 @@ if auth_type == "auth":
     auth = Auth()
 if auth_type == "basic_auth":
     auth = BasicAuth()
+if auth_type == "session_auth":
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+SessionAuth class module
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authentification class """
